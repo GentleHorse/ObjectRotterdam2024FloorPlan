5b193f97ebf5af8aa3a6b54b06552507b47e98f2
GentleHorse <anch.abc.t@gmail.com>
2024-01-22 16:15:42 +0100

    Upload haka building glb model with texture

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,20 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { useGLTF } from "@react-three/drei";
 
 export default function Experience() {
+  const { nodes } = useGLTF("./model/haka-building/building.glb");
+  const bakedHakaBuildingTexture = useTexture("./model/haka-building/baked.jpg");
+  bakedHakaBuildingTexture.flipY = false;
+
   return (
     <>
       <OrbitControls makeDefault />
 
       <Perf position="top-left" />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <mesh geometry={nodes.hakaBuilding.geometry} rotation={[0, Math.PI * 0.5, 0]}>
+        <meshBasicMaterial map={bakedHakaBuildingTexture} />
       </mesh>
     </>
   );
