dd6d00800c7796eb59e515c9bf8865d96114be54
GentleHorse <anch.abc.t@gmail.com>
2024-01-23 00:08:52 +0100

    Added works toshi models

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,41 +1,71 @@
-import { OrbitControls, Sky, useTexture } from "@react-three/drei";
+import { useState } from "react";
+import {
+  Environment,
+  useMatcapTexture,
+  OrbitControls,
+  useGLTF,
+  useTexture,
+} from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { useGLTF } from "@react-three/drei";
 
 import Camera from "./components/utils/Camera.jsx";
 
 export default function Experience() {
-  const { nodes } = useGLTF("./model/haka-building/building.glb");
+  // Haka building
+  const hakaBuilding = useGLTF("./models/haka-building/building.glb");
   const bakedHakaBuildingTexture = useTexture(
-    "./model/haka-building/baked.jpg"
+    "./models/haka-building/baked.jpg"
   );
   bakedHakaBuildingTexture.flipY = false;
+
+  // Works - Toshi
+  const worksToshi = useGLTF("./models/works-toshi/worksToshi.glb");
+  const bakedWorksTE_Texture = useTexture(
+    "./models/works-toshi/bakedWorksTE.jpg"
+  );
+  bakedWorksTE_Texture.flipY = false;
 
   return (
     <>
       <OrbitControls makeDefault />
-
       <Perf position="top-left" />
 
       <axesHelper />
 
-      <Sky
-        sunPosition={[1, 2, 3]}
-        turbidity={10}
-        rayleigh={3}
-        mieCoefficient={0.005}
-        mieDirectionalG={0.7}
-        azimuth={180}
+      <directionalLight
+        castShadow
+        position={[4, 4, 1]}
+        intensity={4.5}
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={10}
+        shadow-camera-right={10}
+        shadow-camera-bottom={-10}
+        shadow-camera-left={-10}
       />
-      
+      <ambientLight intensity={1.5} />
+
       <Camera />
 
+      <Environment background preset="night" />
+
       <mesh
-        geometry={nodes.hakaBuilding.geometry}
-        position={[0, -10, 0]}
+        geometry={hakaBuilding.nodes.hakaBuilding.geometry}
+        scale={0.5}
+        position={[0, -5, 0]}
         rotation={[0, Math.PI * 0.5, 0]}
       >
         <meshBasicMaterial map={bakedHakaBuildingTexture} />
+      </mesh>
+
+      <mesh
+        geometry={worksToshi.nodes.bakedWorksToshi.geometry}
+        scale={0.5}
+        position={[-10, -5, 5]}
+        rotation={[0, -Math.PI * 0.5, 0]}
+      >
+        <meshBasicMaterial map={bakedWorksTE_Texture} />
       </mesh>
     </>
   );
--- src/components/utils/Camera.jsx
+++ src/components/utils/Camera.jsx
@@ -5,9 +5,9 @@ import { useControls } from "leva";
 export default function Camera() {
   const { camera } = useThree();
   const { PosX, PosY, PosZ } = useControls("camera", {
-    PosX: { value: 0, min: -50, max: 50, step: 0.01 },
-    PosY: { value: 1, min: -50, max: 50, step: 0.01 },
-    PosZ: { value: 10, min: -50, max: 50, step: 0.01 },
+    PosX: { value: 0, min: -100, max: 100, step: 0.01 },
+    PosY: { value: 1, min: -100, max: 100, step: 0.01 },
+    PosZ: { value: 40, min: -100, max: 100, step: 0.01 },
   });
 
   useEffect(() => {
