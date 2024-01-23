2262c6c2b41eb9d5d053efaf86c994e5eb7c9bb0
GentleHorse <anch.abc.t@gmail.com>
2024-01-23 21:12:41 +0100

    Added works Jeroen

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,6 +6,7 @@ import {
   useTexture,
   Clone,
 } from "@react-three/drei";
+import * as THREE from "three";
 import { Perf } from "r3f-perf";
 
 import Camera from "./components/utils/Camera.jsx";
@@ -40,6 +41,37 @@ export default function Experience() {
   );
   const wallObject = useGLTF("./models/works-satomi/wallObject.glb");
 
+  // Works - Jeroen
+  const piece01 = useGLTF("./models/works-jeroen/exportJBPiece01.glb");
+  const bakedPiece01_Texture = useTexture(
+    "./models/works-jeroen/bakedWorksJBPiece01.jpg"
+  );
+  bakedPiece01_Texture.flipY = false;
+
+  const piece02 = useGLTF("./models/works-jeroen/exportJBPiece02.glb");
+  const bakedPiece02_Texture = useTexture(
+    "./models/works-jeroen/bakedWorksJBPiece02.jpg"
+  );
+  bakedPiece02_Texture.flipY = false;
+
+  const piece03a = useGLTF("./models/works-jeroen/exportJBPiece03a.glb");
+  const bakedPiece03a_Texture = useTexture(
+    "./models/works-jeroen/bakedWorksJBPiece03a.jpg"
+  );
+  bakedPiece03a_Texture.flipY = false;
+
+  const piece03b = useGLTF("./models/works-jeroen/exportJBPiece03b.glb");
+  const bakedPiece03b_Texture = useTexture(
+    "./models/works-jeroen/bakedWorksJBPiece03b.jpg"
+  );
+  bakedPiece03b_Texture.flipY = false;
+
+  const piece04 = useGLTF("./models/works-jeroen/exportJBPiece04.glb");
+  const bakedPiece04_Texture = useTexture(
+    "./models/works-jeroen/bakedWorksJBPiece04.jpg"
+  );
+  bakedPiece04_Texture.flipY = false;
+
   return (
     <>
       <OrbitControls makeDefault />
@@ -65,7 +97,7 @@ export default function Experience() {
 
         <mesh
           geometry={worksToshi.nodes.bakedWorksToshi.geometry}
-          position={[-18, 0, 15]}
+          position={[-18, 0, 12]}
           rotation={[0, -Math.PI * 0.5, 0]}
         >
           <meshBasicMaterial map={bakedWorksTE_Texture} />
@@ -110,6 +142,58 @@ export default function Experience() {
             position={[-34, 12.5, -4.75]}
           />
         </group>
+
+        <mesh
+          geometry={piece01.nodes.exportJBPiece01.geometry}
+          rotation={[0, -Math.PI * 0.5, 0]}
+          position={[15, 12.5, 28]}
+        >
+          <meshBasicMaterial
+            map={bakedPiece01_Texture}
+            side={THREE.DoubleSide}
+          />
+        </mesh>
+
+        <mesh
+          geometry={piece02.nodes.exportJBPiece02.geometry}
+          rotation={[0, -Math.PI * 0.5, 0]}
+          position={[10, 12.5, 20]}
+        >
+          <meshBasicMaterial
+            map={bakedPiece02_Texture}
+            side={THREE.DoubleSide}
+          />
+        </mesh>
+
+        <mesh
+          geometry={piece03a.nodes.exportJBPiece03a.geometry}
+          rotation={[0, -Math.PI * 0.5, 0]}
+          position={[15, 12.5, -5]}
+        >
+          <meshBasicMaterial map={bakedPiece03a_Texture} />
+        </mesh>
+
+        <mesh
+          geometry={piece03b.nodes.exportJBPiece03b.geometry}
+          rotation={[0, -Math.PI * 0.5, 0]}
+          position={[70, 12.5, -5]}
+        >
+          <meshBasicMaterial
+            map={bakedPiece03b_Texture}
+            side={THREE.DoubleSide}
+          />
+        </mesh>
+
+        <mesh
+          geometry={piece04.nodes.exportJBPiece04.geometry}
+          rotation={[0, -Math.PI * 0.5, 0]}
+          position={[25, 12.5, -20]}
+        >
+          <meshBasicMaterial
+            map={bakedPiece04_Texture}
+            side={THREE.DoubleSide}
+          />
+        </mesh>
       </group>
     </>
   );
