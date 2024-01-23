83c8ffda940eb5dfa02fde4a1464ed8a3574ee2f
GentleHorse <anch.abc.t@gmail.com>
2024-01-23 13:32:35 +0100

    Added works satomi

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import {
   Environment,
-  useMatcapTexture,
   OrbitControls,
   useGLTF,
   useTexture,
+  Clone,
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
@@ -25,48 +25,92 @@ export default function Experience() {
   );
   bakedWorksTE_Texture.flipY = false;
 
+  // Works - Satomi
+  const inflatableStool01 = useGLTF(
+    "./models/works-satomi/inflatableStool01.glb"
+  );
+  const inflatableStool02 = useGLTF(
+    "./models/works-satomi/inflatableStool02.glb"
+  );
+  const edgeColorStool01 = useGLTF(
+    "./models/works-satomi/edgeColorStool01.glb"
+  );
+  const edgeColorStool02 = useGLTF(
+    "./models/works-satomi/edgeColorStool02.glb"
+  );
+  const wallObject = useGLTF("./models/works-satomi/wallObject.glb");
+
   return (
     <>
       <OrbitControls makeDefault />
       <Perf position="top-left" />
 
-      <axesHelper />
+      <axesHelper args={[30]} />
 
-      <directionalLight
-        castShadow
-        position={[4, 4, 1]}
-        intensity={4.5}
-        shadow-mapSize={[1024, 1024]}
-        shadow-camera-near={1}
-        shadow-camera-far={10}
-        shadow-camera-top={10}
-        shadow-camera-right={10}
-        shadow-camera-bottom={-10}
-        shadow-camera-left={-10}
-      />
+      <directionalLight position={[4, 4, 1]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
       <Camera />
 
       <Environment background preset="night" />
 
-      <mesh
-        geometry={hakaBuilding.nodes.hakaBuilding.geometry}
-        scale={0.5}
-        position={[0, -5, 0]}
-        rotation={[0, Math.PI * 0.5, 0]}
-      >
-        <meshBasicMaterial map={bakedHakaBuildingTexture} />
-      </mesh>
+      <group position={[0, -5, 0]} scale={1}>
+        <mesh
+          geometry={hakaBuilding.nodes.hakaBuilding.geometry}
+          position={[0, 0, 0]}
+          rotation={[0, Math.PI * 0.5, 0]}
+        >
+          <meshBasicMaterial map={bakedHakaBuildingTexture} />
+        </mesh>
 
-      <mesh
-        geometry={worksToshi.nodes.bakedWorksToshi.geometry}
-        scale={0.5}
-        position={[-10, -5, 5]}
-        rotation={[0, -Math.PI * 0.5, 0]}
-      >
-        <meshBasicMaterial map={bakedWorksTE_Texture} />
-      </mesh>
+        <mesh
+          geometry={worksToshi.nodes.bakedWorksToshi.geometry}
+          position={[-18, 0, 15]}
+          rotation={[0, -Math.PI * 0.5, 0]}
+        >
+          <meshBasicMaterial map={bakedWorksTE_Texture} />
+        </mesh>
+
+        <group position={[0, 0, 0]}>
+          <primitive object={inflatableStool01.scene} position={[2, 0, -20]} />
+
+          <primitive object={inflatableStool02.scene} position={[-4, 0, -4]} />
+
+          <primitive object={edgeColorStool01.scene} position={[-12, 0, -16]} />
+
+          <primitive object={edgeColorStool02.scene} position={[-20, 0, -8]} />
+
+          <Clone
+            object={wallObject.scene}
+            rotation={[0, Math.PI, 0]}
+            position={[-30, 18.5, -1]}
+          />
+
+          <Clone
+            object={wallObject.scene}
+            rotation={[0, Math.PI, 0]}
+            position={[-30, 14, 0.1]}
+          />
+
+          <Clone
+            object={wallObject.scene}
+            rotation={[0, -Math.PI * 0.5, 0]}
+            position={[-33, 20, -4.75]}
+          />
+
+          <Clone
+            object={wallObject.scene}
+            rotation={[0, -Math.PI * 0.5, 0]}
+            position={[-36, 17, -4.75]}
+          />
+
+          <Clone
+            object={wallObject.scene}
+            rotation={[0, -Math.PI * 0.5, 0]}
+            position={[-34, 12.5, -4.75]}
+          />
+        </group>
+      </group>
     </>
   );
 }
